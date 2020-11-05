e7ba511b97fb9e99aae8408d9f8587e3e918c07c
Julia Giaro <72507630+RevolverGoesPang@users.noreply.github.com>
2020-11-05 14:41:57 +0100

    Initial commit

---
+++ src/DrawPolygons.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+
+public class DrawPolygons extends Component{
+    private ArrayList<String> polygonNames;
+    private ArrayList<Point>  centerPoints;
+
+    public DrawPolygons(){
+        polygonNames = new ArrayList<>(10);//maybe write all the names here instead of creating a hard coded list. Can always add more
+        centerPoints = new ArrayList<>(10);
+
+        polygonNames.add("square");
+        polygonNames.add("triangle");
+        polygonNames.add("rectangle");
+
+        centerPoints.add(new Point(100,100));
+        centerPoints.add(new Point(150,150));
+        centerPoints.add(new Point(100,200));
+    }//constructor
+
+    @Override
+    public void paint(Graphics g) {//maybe just have a polygon method instead that takes in everything? so intstead of typing out everyhting we can minimize it.
+        for (int i = 0; i < polygonNames.size(); i++) {
+            String currentPolygon = polygonNames.get(i);
+            Point currentCenter = centerPoints.get(i);
+
+            if (currentPolygon.equals("square"))
+                g.drawRect(currentCenter.x -10, currentCenter.y -10, 20, 20);
+
+            else if (currentPolygon.equals("triangle")) {
+                g.drawLine(currentCenter.x, currentCenter.y-10, currentCenter.x-10, currentCenter.y+10);//instead of current center write cC and make a method that takes in an offset instead.
+                g.drawLine(currentCenter.x-10, currentCenter.y+10,
+                        currentCenter.x+10, currentCenter.y+10);
+                g.drawLine(currentCenter.x+10, currentCenter.y+10, currentCenter.x, currentCenter.y-10);
+
+            } else if (currentPolygon.equals("rectangle"))
+                g.drawRect(currentCenter.x -20, currentCenter.y -10, 40, 20);
+        }
+    }//paint
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        DrawPolygons polygons = new DrawPolygons();
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(30,30,300,300);
+        frame.getContentPane().add(polygons);
+        frame.setVisible(true);
+
+    }//main
+
+}//DrawPolygons
